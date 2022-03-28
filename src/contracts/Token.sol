990d486e15f77898f5eccde76e91b82a16ef8e8c
pavva91 <valerio.mattioli@protonmail.com>
2022-03-28 17:48:51 +0200

    Emit Transfer Event

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,10 +5,14 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Token {
     using SafeMath for uint;
 
+    // Variables
     string public name = "Pavva Token";
     string public symbol = "PAV";
     uint256 public decimals = 18;
     uint256 public totalSupply;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     // Track balances
     mapping(address => uint256) public balanceOf;
@@ -22,5 +26,6 @@ contract Token {
     function transfer(address _to, uint256 _value) public returns (bool success) {
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
     }
 }   
